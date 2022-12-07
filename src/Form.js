b26e3d4b05d0a45d431a7c09ad5eb7c5847f932c
BIANCA GONZALEZ <bgonzzz@BIANCAs-MBP.home>
2022-12-06 21:53:52 -0800

    delete comments

--- src/Form.js
+++ src/Form.js
@@ -1,26 +1,3 @@
-// import './App.css';
-
-// function Form() {
-//   return (
-//     <div className="FormStructure">
-//       <header className="Form-header">
-//       </header>
-//       <div>
-//         <p>Message Profile Identifier</p>
-//         <p>Maternal Information Section</p>
-//         <p>Maternal Laboratory Interpretive Information</p>
-//         <p>Clinical Signs</p>
-//         <p>Epidemiologic Lab Interpretative questions repeating group -Mother and Infant</p>
-//         <p>Infant Information</p>
-//         <p>Other Infant Laboratory Interpretive Information</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Form;
-
-/* src/App.js */
 import React, { useEffect, useState } from 'react'
 import { Amplify, API, graphqlOperation } from 'aws-amplify'
 import { createMMG } from './graphql/mutations'
