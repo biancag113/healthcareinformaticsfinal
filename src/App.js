471267f7a6645433af4dfa2542a7763ee4fd76f6
BIANCA GONZALEZ <bgonzzz@BIANCAs-MBP.home>
2022-12-06 19:20:23 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CDC - Congenital Syphillis Reporting
+        </p>
+        <Form>
+        </ Form>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function Form() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default Form;
