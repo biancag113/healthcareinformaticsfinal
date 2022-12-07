72d1e7a504b27cb959569b861baaf5d4597829ab
BIANCA GONZALEZ <bgonzzz@BIANCAs-MBP.home>
2022-12-06 20:04:30 -0800

    categories questions

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,15 @@ import './App.css';
 
 function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          
-        </p>
+    <div className="FormStructure">
+      <header className="Form-header">
+        <p>Message Profile Identifier</p>
+        <p>Maternal Information Section</p>
+        <p>Maternal Laboratory Interpretive Information</p>
+        <p>Clinical Signs</p>
+        <p>Epidemiologic Lab Interpretative questions repeating group -Mother and Infant</p>
+        <p>Infant Information</p>
+        <p>Other Infant Laboratory Interpretive Information</p>
 
       </header>
     </div>
